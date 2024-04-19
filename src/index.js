b0ddc18b81dc6d8b8970b17f6cb20b677a8b8242
SonjaCodes <sonja@kolmer.cc>
2024-04-19 21:24:51 +0200

    added id to form to call from js + added AI and function to return result

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
+function displayQuote(response) {
+  new Typewriter("#quote", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 20,
+    cursor: "",
+  });
+}
+
 function generateQuote(event) {
   event.preventDefault();
 
-  new Typewriter("#quote", {
-    strings: "Your inspiration is in the works...",
-    autoStart: true,
-    delay: 20,
-    cursor: null,
-    loop: true,
-  });
+  let instructionInput = document.querySelector("#instructions");
+  let apiKey = "fbef01f4et1b02o0d25c27210a43ef3f";
+  let context =
+    "You are an inspiration to everyone around the globe and an expert on quotes from famous people.  You love to write short inspirational quotes.  Your mission is to generate a short cheerful inspirational quote in basic HTML.  Make sure to follow the user instructions.";
+  let prompt = `Generate an inspirational quote about ${instructionInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  quote.innerHTML = "Your inspiration is in the works...";
+
+  axios.get(apiURL).then(displayQuote);
 }
 
 let inspirationFormElement = document.querySelector("#inspirational-form");
