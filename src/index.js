0151b9ad78fdb7e02cc82be2ccc9fb23980c84b3
SonjaCodes <sonja@kolmer.cc>
2024-04-21 12:23:56 +0200

    improved UX

--- src/index.js
+++ src/index.js
@@ -13,11 +13,13 @@ function generateQuote(event) {
   let instructionInput = document.querySelector("#instructions");
   let apiKey = "fbef01f4et1b02o0d25c27210a43ef3f";
   let context =
-    "You are an inspiration to everyone around the globe and an expert on quotes from famous people.  You love to write short inspirational quotes.  Your mission is to generate a short cheerful inspirational quote in basic HTML.  Please do not mention the format, simply the quote itself. Make sure to follow the user instructions. Sign it with 'SheCodes AI' inside a <strong> element after a <br>.";
+    "You are an inspiration to everyone around the globe and an expert on quotes from famous people.  You love to write short inspirational quotes.  Your mission is to generate a short cheerful inspirational quote in basic HTML.  Please do not mention the format, simply the quote itself. Make sure to follow the user instructions. Sign it with '🤖 SheCodes AI' inside a <strong> element.";
   let prompt = `Generate an inspirational quote about ${instructionInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  quote.innerHTML = "Your inspiration is in the works...";
+  let quoteElement = document.querySelector("#quote");
+  quoteElement.classList.remove("hidden");
+  quote.innerHTML = `<div class="generating">⏱️ Your inspiration is in the works about ${instructionInput.value}...</div>`;
 
   axios.get(apiURL).then(displayQuote);
 }
